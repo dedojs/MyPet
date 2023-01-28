6cadcb7d13e13392cf87060318d5eb373ff30781
dedojs <xandresousax@gmail.com>
2023-01-28 16:42:46 -0300

    Feat: create models

---
+++ MyPet/Models/Tutor.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPet.Models
+{
+    public class Tutor
+    {
+        [Key]
+        public int TutorId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cep { get; set;}
+        public string Password { get; set; }
+        public List<Pet> Pets { get; set; }
+
+
+    }
+}
